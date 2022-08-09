29756cf0ca4e82fbace7139d9e5523d73309f956
kreindo <ahmadsan4p@gmail.com>
2022-08-09 18:12:22 +0800

    added more functionality

--- src/Components/Feeds.jsx
+++ src/Components/Feeds.jsx
@@ -1,22 +1,54 @@
 import React from 'react';import { useState, useEffect } from 'react';
+const defaultNews = {
+  status: 'ok',
+  totalResult: 0,
+  articles: [],
+};
+const endpoint =
+  'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=cad82b04d8734204ade8411de722f112https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=cad82b04d8734204ade8411de722f112';
 const Feeds = () => {
-  const [state, setState] = useState({
-    name: '',
-    age: 0,
-    isMarried: null,
+  const [news, setNews] = useState(defaultNews);
+  const [page, setPage] = useState(0);
+  const [isLoading, setLoading] = useState(false);
+  const [isError, setError] = useState(false);
+  const [isRefresh, setRefresh] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(`${endpoint}&page=${page}`);
+        const result = await response.json();
+        setNews((current) => {
+          return {
+            ...result,
+            articles: [...current.articles, ...result.articles],
+            totalResult: result.totalResult,
+            status: result.status,
+          };
+        });
+        if (result.status != 'ok') {
+          throw new Error('error');
+        }
+      } catch (error) {
+        setError(true);
+      }
+      setLoading(false);
+    };
+    fetchData();
   });
-
-  const handleState = () => {
-    setState((currentState.age) => currentState.age + 1);
-  };
-
   return (
     <>
-      <h2>react hooks</h2>
-      <p>{state.name}</p>
-      <p>{state.age}</p>
-      <button onClick={() => handleState()}>age ++</button>
-      <p>{JSON.stringify(state.isMarried)}</p>
+      <h3>Nest Feed</h3>
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>Error thrown</p>}
+      <ol>
+        {news.articles.map((item, index) => {
+          <li key={index}>{item.title}</li>;
+        })}
+      </ol>
+      <button disabled={isLoading}>Load more...</button>
+      <button disabled={isLoading}>Refresh</button>
     </>
   );
 };
